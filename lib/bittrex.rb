865a60a2180c62dbabb2e7c251e4cf0660ba0edb
Fernando Serrano <fernandos@eventbrite.com>
2017-09-24 11:46:04 -0300

    make Market optional for open_orders

--- lib/bittrex.rb
+++ lib/bittrex.rb
@@ -60,17 +60,26 @@ module Bittrex
       request("https://bittrex.com/api/#{API_VERSION}/market/cancel", "uuid=#{order_id}")
     end
 
-    def open_orders(market = '')
-      request("https://bittrex.com/api/#{API_VERSION}/market/getopenorders", "market=#{market}")
+    def open_orders(market = nil)
+      if market then
+        request("https://bittrex.com/api/#{API_VERSION}/market/getopenorders", "market=#{market}")
+      else
+        request("https://bittrex.com/api/#{API_VERSION}/market/getopenorders")
+      end
     end
 
-    # Market actions related methods
+    # End of market actions related methods
 
 
     # Personal account methods
 
-    def balance(currency)
-      request("https://bittrex.com/api/#{API_VERSION}/account/getbalance", "currency=#{currency}")
+    def balance(currency = nil)
+      if currency then
+        request("https://bittrex.com/api/#{API_VERSION}/account/getbalance", "currency=#{currency}")
+      else
+        request("https://bittrex.com/api/#{API_VERSION}/account/getbalance")
+      end
+
     end
 
     def order_history(market = nil, count = 10)
@@ -84,7 +93,12 @@ module Bittrex
 
       def generate_sign(url, params)
         nonce = Time.now.to_i
-        @final_url = "#{url}?apikey=#{@api_key}&nonce=#{nonce}&"+params
+
+        @final_url = "#{url}?apikey=#{@api_key}&nonce=#{nonce}"
+        if !params then
+          @final_url += '&'+params
+        end
+
         OpenSSL::HMAC.hexdigest(digest = OpenSSL::Digest.new('sha512'), @api_secret, @final_url)
       end
 
@@ -97,7 +111,7 @@ module Bittrex
         end
       end
 
-      def request(url, params = '')
+      def request(url, params = nil)
         begin
           hmac_sign = generate_sign(url, params)
           handle_response open(@final_url, 'apisign' => hmac_sign)
